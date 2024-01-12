3163c206872a662e082fd51b5650d00213fc77ad
Magemaster506 <70059769+Magemaster506@users.noreply.github.com>
2024-01-12 19:18:47 +1100

    12/1/24 - Commit 1
    Base mechanics implemented, next commit should include enemy movement and i will either rework and implement my particle system code or i will just write a new system.

---
+++ Hurricane/data/settings.py
@@ -0,0 +1,41 @@
+FPS = 60
+
+WIN_HEIGHT = 800
+WIN_WIDTH = 800
+WIN_TITLE = 'Hurricane'
+
+PLAYER_IMAGE = 'data/images/entities/player/player.png'
+PISTOL_IMAGE = 'data/images/weapons/Rifle.png'
+FLIPPED_PISTOL_IMAGE = 'data/images/weapons/pistol_flipped.png'
+BACKGROUND_IMAGE = 'data/images/background.png'
+
+UI_IMAGE = 'data/images/UI.png'
+
+PLAYER_SPEED = 2
+BULLET_ACCURACY = 6
+BULLET_TRAVEL_SPEED = 15
+FIRE_RATE = 10
+
+BASE_ENEMY_HEALTH = 70
+ENEMY_KNOCKBACK = 4
+
+NORTH_FRAME1 = 'data/images/entities/player/north/northframe1.png'
+NORTH_FRAME2 = 'data/images/entities/player/north/northframe2.png'
+NORTH_FRAME3 = 'data/images/entities/player/north/northframe1.png'
+NORTH_FRAME4 = 'data/images/entities/player/north/northframe3.png'
+
+EAST_FRAME1 = 'data/images/entities/player/east/eastframe1.png'
+EAST_FRAME2 = 'data/images/entities/player/east/eastframe2.png'
+EAST_FRAME3 = 'data/images/entities/player/east/eastframe1.png'
+EAST_FRAME4 = 'data/images/entities/player/east/eastframe3.png'
+
+SOUTH_FRAME1 = 'data/images/entities/player/south/southframe1.png'
+SOUTH_FRAME2 = 'data/images/entities/player/south/southframe2.png'
+SOUTH_FRAME3 = 'data/images/entities/player/south/southframe1.png'
+SOUTH_FRAME4 = 'data/images/entities/player/south/southframe3.png'
+
+WEST_FRAME1 = 'data/images/entities/player/west/westframe1.png'
+WEST_FRAME2 = 'data/images/entities/player/west/westframe2.png'
+WEST_FRAME3 = 'data/images/entities/player/west/westframe1.png'
+WEST_FRAME4 = 'data/images/entities/player/west/westframe3.png'
+
